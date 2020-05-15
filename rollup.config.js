8e6d207cc3b5ae9e355f390e3da5ce5888fdd2ba
Samuel Gratzl <sam@sgratzl.com>
2020-05-15 19:32:35 +0200

    update setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,32 @@
 // rollup.config.js
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import pnp from 'rollup-plugin-pnp-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 
 export default [
   {
     output: {
-      file: 'build/Chart.ErrorBars.js',
-      name: 'ChartErrorBars',
+      file: 'build/Chart.Geo.js',
+      name: 'ChartGeo',
       format: 'umd',
       globals: {
         'chart.js': 'Chart',
       },
     },
     external: ['chart.js'],
-    plugins: [resolve(), commonjs(), babel()],
+    plugins: [commonjs(), pnp(), resolve(), babel({ babelHelpers: 'runtime' })],
   },
   {
     output: {
-      file: 'build/Chart.ErrorBars.esm.js',
-      name: 'ChartErrorBars',
+      file: 'build/Chart.Geo.esm.js',
+      name: 'ChartGeo',
       format: 'esm',
       globals: {
         'chart.js': 'Chart',
       },
     },
-    external: ['chart.js'],
-    plugins: [resolve(), commonjs(), babel()],
+    external: ['chart.js', '@babel/runtime', 'd3-geo', 'd3-scale-chromatic', 'topojson-client'],
+    plugins: [commonjs(), pnp(), resolve(), babel({ babelHelpers: 'runtime' })],
   },
 ];
