67997c4a502272ec48942adb7e2471e22c6f24fc
Samuel Gratzl <sam@sgratzl.com>
2022-08-27 14:03:42 -0400

    fix: line with linear scale
    closes #30

--- src/controllers/LineWithErrorBarsController.spec.ts
+++ src/controllers/LineWithErrorBarsController.spec.ts
@@ -3,7 +3,7 @@ import createChart from '../__tests__/createChart';
 import { LineWithErrorBarsController } from './LineWithErrorBarsController';
 import { PointWithErrorBar } from '../elements';
 
-describe('bar', () => {
+describe('line', () => {
   beforeAll(() => {
     registry.addControllers(LineWithErrorBarsController);
     registry.addElements(PointWithErrorBar);
@@ -34,6 +34,43 @@ describe('bar', () => {
       options: {
         scales: {
           x: {
+            display: false,
+          },
+          y: {
+            display: false,
+          },
+        },
+      },
+    }).toMatchImageSnapshot();
+  });
+
+  test('linear scale', () => {
+    return createChart({
+      type: LineWithErrorBarsController.id,
+      data: {
+        datasets: [
+          {
+            data: [
+              {
+                x: 1,
+                y: 4,
+                yMin: 1,
+                yMax: 6,
+              },
+              {
+                x: 2,
+                y: 2,
+                yMin: 1,
+                yMax: 4,
+              },
+            ],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
             display: false,
           },
           y: {
